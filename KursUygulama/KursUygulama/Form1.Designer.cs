f19f94ee94eb5c0e472e92631a188a329c345b72
GurkanEyupoglu <119728194+GurkanEyupoglu@users.noreply.github.com>
2023-09-21 17:20:31 +0300

    Update Form1.Designer.cs

--- KursUygulama/KursUygulama/Form1.Designer.cs
+++ KursUygulama/KursUygulama/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace KursUygulama
+namespace KursUygulama
 {
     partial class Form1
     {
@@ -63,7 +63,7 @@
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(69, 19);
             this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "Mustafa";
+            this.checkBox1.Text = "Hakan";
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
