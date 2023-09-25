c721ba9dc3d519cf1770527e87a63c6f826c8fc1
GurkanEyupoglu <119728194+GurkanEyupoglu@users.noreply.github.com>
2023-09-25 19:09:32 +0300

    Update Program.cs

--- KursUygulama/KursUygulama/Program.cs
+++ KursUygulama/KursUygulama/Program.cs
@@ -8,6 +8,24 @@ namespace KursUygulama
         [STAThread]
         static void Main()
         {
+     msc.main
+     {
+    <service.response>
+    <creditline wmidfrom="WMID_FROM" wmidfor="WMID_FOR">
+      <purse>PURSE</purse>
+      <amountmax>AMOUNTMAX</amountmax>
+      <amountpay>AMOUNTPAY</amountpay>
+      <ret_period>RET_PERIOD</ret_period>
+      <ret_proc>RET_PROC</ret_proc>
+      <ret_per>RET_PER</ret_per>
+      <state>STATE</state>
+      <datecrt>DATECRT</datecrt>
+      <dateupd>DATEUPD</dateupd>
+    </creditline>
+  <retval>RETVAL</retval>
+  <retdesc>RETDESC</retdesc>
+</service.response>
+}
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
